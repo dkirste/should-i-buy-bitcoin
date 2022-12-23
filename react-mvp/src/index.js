8d606c4090d55d9c734989aa96c0e233793c4f66
Daniel Kirste <danielkirste@icloud.com>
2022-12-23 15:31:30 +0100

    Updated css

--- react-mvp/src/index.js
+++ react-mvp/src/index.js
@@ -8,23 +8,24 @@ import {
     Area,
     CartesianGrid,
     XAxis,
-    YAxis,
+    YAxis, Tooltip,
 } from 'recharts';
 
 
 const data = [{time: '2022-12-23T13:30:34.110567+00:00', value: 400}, {time: '2022-12-23T13:31:34.110567+00:00', value: 200}, {time: '2022-12-23T13:32:34.110567+00:00', value: 500},];
 
 const TimeSeriesChart = (
-    <ResponsiveContainer width="100%" height={400}>
+    <ResponsiveContainer width="80%" height={400} className="center">
         <AreaChart data={data}>
             <defs>
                 <linearGradient id="color" x1="0" y1="0" x2="0" y2="1">
-                    <stop offset="0%" stopColor="#2451B7" stopOpacity={0.4} />
-                    <stop offset="75%" stopColor="#2451B7" stopOpacity={0.05} />
+                    <stop offset="0%" stopColor="#FF9900" stopOpacity={0.4} />
+                    <stop offset="75%" stopColor="#FF9900" stopOpacity={0.05} />
                 </linearGradient>
             </defs>
 
-            <Area dataKey="value" stroke="#2451B7" fill="url(#color)" />
+            <Area dataKey="value" stroke="#FF9900" fill="url(#color)" />
+
 
             <XAxis
                 dataKey="time"
@@ -47,6 +48,7 @@ const TimeSeriesChart = (
                 tickFormatter={(number) => `$${number.toFixed(2)}`}
             />
 
+            <Tooltip/>
 
             <CartesianGrid opacity={0.1} vertical={false} />
         </AreaChart>
@@ -55,13 +57,12 @@ const TimeSeriesChart = (
 
 
 
-
 const myFirstElement = <html>
                         <head>
                             <title>Should I buy Tesla?</title>
                         </head>
                         <body>
-                            <h1 id="centerText">Should I buy Tesla?</h1>
+                            <h1 id="centerText">Should I buy Bitcoin? -> YES</h1>
                             {TimeSeriesChart}
                         </body>
                         </html>
